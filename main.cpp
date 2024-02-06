dbde3dbe143c8786e692f786277d1299b57faf54
Louis Franceschi <louisbart3@gmail.com>
2024-02-06 19:40:52 +0100

    code fix

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void welcome();
+int chooseNumber();
+bool checkAnswer(int num);
+
+int main()
+{
+    bool play{true};
+    welcome();
+
+    do
+    {
+        int number{chooseNumber()};
+        std::cout << "You chose " << number << '\n';
+    } while (play);
+
+    return 0;
+}
+
+void welcome()
+{
+    std::cout << "Welcome to the number guessing game!\n";
+    std::cout << "Guess a number between 0 and 100. You have 6 chances!\n";
+}
+
+int chooseNumber()
+{
+    std::cout << "Enter a number 0-100: ";
+    int x{};
+    std::cin >> x;
+
+    return x;
+}
